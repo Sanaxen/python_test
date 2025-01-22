d65e202919e53153005a88f96825c9ef26f3d40c
Sanaxen <bccatfalcon@gmail.com>
2025-01-22 21:38:37 +0900

    Update make_base_env_name_Jupyter_Notebook.bat

--- setting_bat/make_base_env_name_Jupyter_Notebook.bat
+++ setting_bat/make_base_env_name_Jupyter_Notebook.bat
@@ -2,6 +2,7 @@ if "%1"=="" goto err
 
 call make_base_env_name.bat "%1_note"
 
+call "%1_note"\Scripts\activate.bat
 
 pip install --upgrade pip
 python -m pip install --upgrade pip
@@ -10,7 +11,10 @@ pip install notebook
 python -m pip install notebook
 python -m pip install --upgrade notebook
 
-echo start jupyter notebook >"%1_note_execute.bat"
+python3 -m ipykernel install --user --name myenv --display-name "%1_note"
+
+echo call "%1_note"\Scripts\activate.bat >"%1_note_execute.bat"
+echo start jupyter notebook >>"%1_note_execute.bat"
 
 :仮想環境のコピー
 python -m pip freeze > %1_note_requirements.txt
